82750ab39c7f54d8c2ff817ac359225734727b90
Andrew Edmonds <andrew.edmonds11@gmail.com>
2023-01-27 10:41:16 -0600

    Add combinations of issues

--- routes/greater.js
+++ routes/greater.js
@@ -10,7 +10,19 @@ router
       //      .toArray();
 
       // use 'cardinality' instead of 'array_length' because we want an empty array to return 0
-      let query = 'SELECT * FROM panels WHERE cardinality('+req.params.uw_issue+')'+req.params.uw_op+req.params.uw_value;
+      let uw_issue="";
+      if (req.params.uw_issue == "any_issues") {
+	  uw_issue = "array_cat(missing_straws, array_cat(blocked_straws, high_current_wires))";
+      }
+      else if (req.params.uw_issue == "unusable_channels") {
+	  uw_issue = "missing_straws";
+      }
+      else if (req.params.uw_issue == "semi_unusable_channels") {
+	  uw_issue = "array_cat(blocked_straws, high_current_wires)";
+      }
+      else { uw_issue=req.params.uw_issue; }
+      
+      let query = 'SELECT * FROM panels WHERE cardinality('+uw_issue+')'+req.params.uw_op+req.params.uw_value;
       if (req.params.andor2) {
 	  query += " " + req.params.andor2 + ' cardinality('+req.params.uw_issue2+')'+req.params.uw_op2+req.params.uw_value2;
 	  if (req.params.andor3) {
