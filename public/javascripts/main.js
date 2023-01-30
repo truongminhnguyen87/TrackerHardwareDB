4977ae993e3919a635d8c83bf2c2b6a637d2be3f
Andrew Edmonds <andrew.edmonds11@gmail.com>
2023-01-30 13:04:57 -0600

    Report in the output when a specific panel is not found

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -138,55 +138,61 @@ showButton.addEventListener('click', async function () {
 	undefined,
 	2);
 
-    var this_panel_issues = panel_info[0]
+    var output = "Panel "+panel_number;
+    if (panel_info.length>0) {
+	var this_panel_issues = panel_info[0]
 
-    var all_wires = Array(96).fill(0)
-    var wire_numbers = Array(96).fill(0)
-    for (let i = 0; i < all_wires.length; i++) {
-	wire_numbers[i] = i;
-    }
-
-    var data = Array(all_issues.length)
-    var total_issues = 0
-    for (let i = 0; i < data.length; i++) {
-	var the_issue = all_issues[i];
-	var this_panel_straws = Array(96).fill(0)
-//	console.log(the_issue)
-	var this_panel_issue = this_panel_issues[the_issue];
-	for (let i = 0; i < this_panel_issue.length; i++) {
-	    this_panel_straws[this_panel_issue[i]] = 1;
+	var all_wires = Array(96).fill(0)
+	var wire_numbers = Array(96).fill(0)
+	for (let i = 0; i < all_wires.length; i++) {
+	    wire_numbers[i] = i;
 	}
-	total_issues = total_issues + this_panel_issue.length
-	var this_data = {
-	    name : the_issue,
-	    type : 'bar',
-	    x: wire_numbers,
-	    y: this_panel_straws
-	};
-	data[i] = this_data
-    }
+
+	var data = Array(all_issues.length)
+	var total_issues = 0
+	for (let i = 0; i < data.length; i++) {
+	    var the_issue = all_issues[i];
+	    var this_panel_straws = Array(96).fill(0)
+	    //	console.log(the_issue)
+	    var this_panel_issue = this_panel_issues[the_issue];
+	    for (let i = 0; i < this_panel_issue.length; i++) {
+		this_panel_straws[this_panel_issue[i]] = 1;
+	    }
+	    total_issues = total_issues + this_panel_issue.length
+	    var this_data = {
+		name : the_issue,
+		type : 'bar',
+		x: wire_numbers,
+		y: this_panel_straws
+	    };
+	    data[i] = this_data
+	}
     
-    straw_status_plot = document.getElementById('straw_status_plot');
-    var xaxis = {title : {text : 'straw number'}, tickmode : "linear", tick0 : 0.0, dtick : 1.0, gridwidth : 2};
-    var yaxis = {title : {text : 'no. of issues'}};
-    var layout = { title : {text: this_title + " Straw/Wire Status"},
-		   xaxis : xaxis,
-		   yaxis : yaxis,
-		   barmode : 'stack',
-//		   margin: {t:0},
-		   scroolZoom : true };
-    Plotly.newPlot(straw_status_plot, data, layout);
+	straw_status_plot = document.getElementById('straw_status_plot');
+	var xaxis = {title : {text : 'straw number'}, tickmode : "linear", tick0 : 0.0, dtick : 1.0, gridwidth : 2};
+	var yaxis = {title : {text : 'no. of issues'}};
+	var layout = { title : {text: this_title + " Straw/Wire Status"},
+		       xaxis : xaxis,
+		       yaxis : yaxis,
+		       barmode : 'stack',
+		       //		   margin: {t:0},
+		       scroolZoom : true };
+	Plotly.newPlot(straw_status_plot, data, layout);
 
     // total = missing_straws.length + high_current_wires.length + blocked_straws.length + sparking_wires.length;
-    var output = "Panel "+panel_number+" has "+total_issues+" bad channels: ("
-    for (let i = 0; i < data.length; i++) {
-	var the_issue = all_issues[i];
-	var this_panel_straws = Array(96).fill(0)
-	var this_panel_issue = this_panel_issues[the_issue];
-	output += this_panel_issue.length + " " + the_issue;
-
-	if (i != data.length-1) { output += ", "; }
-	else { output += ")"; }
+	output += " has "+total_issues+" bad channels: ("
+	for (let i = 0; i < data.length; i++) {
+	    var the_issue = all_issues[i];
+	    var this_panel_straws = Array(96).fill(0)
+	    var this_panel_issue = this_panel_issues[the_issue];
+	    output += this_panel_issue.length + " " + the_issue;
+	    
+	    if (i != data.length-1) { output += ", "; }
+	    else { output += ")"; }
+	}
+    }
+    else {
+	output += " not found!";
     }
     document.getElementById("panel_info").innerHTML = output;
 });
