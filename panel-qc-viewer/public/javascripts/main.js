f4f094ee108bbff22860e5b218decccb29e3b582
Andrew Edmonds <awje@bu.edu>
2023-02-24 11:26:08 -0600

    Add HV data exists summary plot

--- panel-qc-viewer/public/javascripts/main.js
+++ panel-qc-viewer/public/javascripts/main.js
@@ -11,8 +11,12 @@ const allPanelInfo = await response.json();
 //console.log(allPanelInfo);
 var single_channel_n_data = Array(single_channel_issues.length)
 var panels = Array(allPanelInfo.length)
+var hv_exists = Array(allPanelInfo.length).fill(0)
 for (let i_panel = 0; i_panel < panels.length; i_panel++) {
     panels[i_panel] = allPanelInfo[i_panel]['id'];
+    if (allPanelInfo[i_panel]['max_erf_fit'].length != 0) {
+	hv_exists[i_panel] = 1;
+    }
 }
 for (let i_issue = 0; i_issue < single_channel_issues.length; ++i_issue) {
     var n_issue = Array(allPanelInfo.length);
@@ -40,6 +44,25 @@ var layout = { title : {text: "All Single-Channel Issues vs Panel Number"},
 	       yaxis : yaxis,
 	       scroolZoom : true };
 Plotly.newPlot(single_channel_issue_vs_panel_plot, single_channel_n_data, layout);
+
+var hv_data_vs_panel_plot = document.getElementById('hv_data_vs_panel_plot');
+var hv_data_exists = {name : "hv_data_exists",
+		      x: panels,
+		      y: hv_exists,
+		      mode:"markers",
+		      type:"scatter"
+		     }
+var xaxis = {title : {text : 'panel number'}, tickmode : "linear", tick0 : 0.0, dtick : 10.0, gridwidth : 2};
+var yaxis = {title : {text : ''}, 
+	     tickmode: "array",
+	     tickvals: [0, 1],
+	     ticktext: ['no', 'yes']
+	    };
+var layout = { title : {text: "HV Data Exists?"},
+	       xaxis : xaxis,
+	       yaxis : yaxis,
+	       scroolZoom : true };
+Plotly.newPlot(hv_data_vs_panel_plot, [ hv_data_exists ], layout);
 
 // var doublet_channel_n_data = Array(doublet_channel_issues.length)
 // var panels = Array(allPanelInfo.length)
