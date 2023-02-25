61ae29f879018b80141a5b512daefe4877b64619
Andrew Edmonds <awje@bu.edu>
2023-02-25 17:12:50 -0600

    Add a couple more issues

--- panel-qc-viewer/public/javascripts/single_channel_issues.js
+++ panel-qc-viewer/public/javascripts/single_channel_issues.js
@@ -1,3 +1,3 @@
 export function single_channel_issues() {
-    return ["missing_straws", "high_current_wires", "blocked_straws", "short_wires", "sparking_wires", "missing_anode", "missing_omega_pieces" ];
+    return ["missing_straws", "missing_wires", "high_current_wires", "blocked_straws", "short_wires", "sparking_wires", "missing_anode", "missing_omega_pieces", "loose_omega_pieces", "low_flow_straws", "no_hv_straw_cal", "no_hv_straw_hv" ];
 }
